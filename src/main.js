e51d0fa1eb0c3632c5459226a6e15aa5eefb75e3
parsa4523 <parsa4523@gmail.com>
2021-05-03 23:44:31 +0430

    Cleaning Up

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
+// Animation on scroll
 AOS.init({
 	duration: 800,
 });
-
-const sectionResume = document.querySelector('#section-resume');
-const sectionProjects = document.querySelector('.section-projects');
-const sectionContact = document.querySelector('.section-contact');
 
 // Header navigation buttons
 const hamburger = document.querySelector('.navbar-burger');
 const navbarMenu = document.querySelector('.navbar-menu');
 
-// Animations Components
-const intros = document.querySelectorAll('.intro');
-const skillCards = document.querySelectorAll('.skill-card');
-const toAnimate = document.querySelectorAll('*[data-anim]');
+// Navigation Components
+const IntroButton = document.querySelector('.intro__btn');
 
 // Header scroll navigation
 navbarMenu.addEventListener('click', function (e) {
@@ -27,35 +23,22 @@ navbarMenu.addEventListener('click', function (e) {
 	}
 });
 
+// CTA scroll navigation
+IntroButton.addEventListener('click', function (e) {
+	const scrollTo = document.querySelector(
+		`.section-${e.target.dataset.section}`
+	);
+	scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
+
 // Hamburger menu button
 hamburger.addEventListener('click', function (e) {
 	this.classList.toggle('is-active');
 	navbarMenu.classList.toggle('is-active');
 });
 
-// Animation
-const revealAnimation = function (entries, observer) {
-	entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			entry.target.classList.remove(`animate--${entry.target.dataset.anim}`);
-		}
-	});
-};
-
-const introObserver = new IntersectionObserver(revealAnimation, { root: null });
-
-// intros.forEach(intro => {
-//     introObserver.observe(intro);
-// });
-
-toAnimate.forEach(skillcard => {
-	introObserver.observe(skillcard);
-});
-
 // Sticky navbar
 const navbar = document.querySelector('.navbar');
-const navHeight = navbar.getBoundingClientRect().height;
-const sectionMe = document.querySelector('.section-me');
 
 const stickyNav = function (entries) {
 	const [entry] = entries;
@@ -70,44 +53,21 @@ const stickyNav = function (entries) {
 	}
 };
 
-const headerObserver = new IntersectionObserver(stickyNav, {
-	root: null,
-	threshold: 0,
-	rootMargin: `-${2 * navHeight}px`,
-});
-
-// headerObserver.observe(sectionMe);
-
 // Slider
-
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.arrow--left');
 const btnRight = document.querySelector('.arrow--right');
-const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 let maxSlide = slides.length - 2;
 
-const createDots = function () {
-	slides.forEach((s, i) => {
-		dotContainer.insertAdjacentHTML(
-			'beforeend',
-			`
-    <button class="dots__dot" data-slide="${i}"></button>
-    `
-		);
-	});
-};
-
-const activateDot = function (slide) {
-	document
-		.querySelectorAll('.dots__dot')
-		.forEach(dot => dot.classList.remove('dots__dot--active'));
-
-	document
-		.querySelector(`.dots__dot[data-slide="${slide}"]`)
-		.classList.add('dots__dot--active');
-};
+window.addEventListener('resize', function () {
+	if (window.innerWidth < 640) {
+		maxSlide = slides.length - 1;
+	} else {
+		maxSlide = slides.length - 2;
+	}
+});
 
 const goToSlide = function (slide) {
 	slides.forEach(
@@ -134,7 +94,6 @@ const prevSlide = function () {
 	} else {
 		curSlide--;
 	}
-
 	goToSlide(curSlide);
 };
 
